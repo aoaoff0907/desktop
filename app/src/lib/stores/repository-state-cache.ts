1d0a0bcaa47609809c4158b340b5b91697be3299
aofaof <aofaof0907@gmail.com>
2021-07-20 01:45:57 +0700

    Merge branch 'bugfix/merge-preview-conflict-count' into development

--- app/src/lib/stores/repository-state-cache.ts
+++ app/src/lib/stores/repository-state-cache.ts
@@ -113,7 +113,7 @@ export class RepositoryStateCache {
 
   public updateRebaseState<K extends keyof IRebaseState>(
     repository: Repository,
-    fn: (branchesState: IRebaseState) => Pick<IRebaseState, K>
+    fn: (rebaseState: IRebaseState) => Pick<IRebaseState, K>
   ) {
     this.update(repository, state => {
       const { rebaseState } = state
